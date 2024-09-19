f2211b3d5c9b41392dffbf76b14f5f2fec012771
CrisGriebler <cristiangrb@gmail.com>
2024-09-19 19:51:02 -0300

    biometria9e10

--- fish/FeatureImp/certosep.py
+++ fish/FeatureImp/certosep.py
@@ -5,12 +5,15 @@ df = pd.read_excel("Variavel_sep.xlsx")
 
 df.columns = df.columns.str.strip()
 
+# Convert necessary columns to numeric values, with coercion for errors
 df['comprimento_Total'] = pd.to_numeric(df['comprimento_Total'], errors='coerce')
 df['peso'] = pd.to_numeric(df['peso'], errors='coerce')
 df['altura'] = pd.to_numeric(df['altura'], errors='coerce')
 
 df['peso_categoria'] = pd.cut(df['peso'], bins=range(0, int(df['peso'].max()) + 100, 100), right=False)
+df['peso_categoria'] = df['peso_categoria'].astype(str)  # Convert intervals to string for plotting
 
+# Create the 3D scatter plot
 fig = px.scatter_3d(df, x='comprimento_Total', y='peso', z='altura', color='peso_categoria',
                     title='Scatter Plot 3D de Peso vs Comprimento Total vs Altura',
                     labels={'peso_categoria': 'Intervalo de Peso'},
